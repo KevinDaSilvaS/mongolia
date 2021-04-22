c2d084bce5d45d1bbf419ec0a70b930d73da58bd
Kevin da Silva <kevin.silva@sensedia.com>
2021-04-22 16:45:29 -0300

    Add custom errors file to application

---
+++ src/constants/ErrorMessages.js
@@ -0,0 +1,6 @@
+module.exports = {
+    BOARD_NOT_FOUND: "Board not found.",
+    ERROR_UPDATE_BOARD: "Unable to update board.",
+    TASK_NOT_FOUND: "Task not found.",
+    ERROR_UPDATE_TASK: "Unable to update task.",
+}

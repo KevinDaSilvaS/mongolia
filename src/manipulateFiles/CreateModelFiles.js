85a669608b695174711dca30ccee57001e23e756
Kevin da Silva <kevin.silva@sensedia.com>
2021-04-22 09:45:53 -0300

    Add file to create mongoose  models

---
+++ src/manipulateFiles/CreateModelFiles.js
@@ -0,0 +1,21 @@
+fs = require('fs');
+
+const createModelFile = ({collectionName, collectionProperties, path, addressPath}) => {
+    let parsedCollectionProperties = JSON.stringify((collectionProperties)).replace(/"/g, '');
+    const contentFile = `
+        const mongoose = require('mongoose');
+    
+        const ${collectionName}Schema = new mongoose.Schema(${parsedCollectionProperties});
+        
+        const ${collectionName}Model = mongoose.model("${collectionName}", ${collectionName}Schema);
+        
+        module.exports = ${collectionName}Model;
+    `;
+
+    fs.writeFile(`${path}/${collectionName}Model.js`, contentFile, function (err) {
+        if (err) throw err;
+        return `${addressPath}/${collectionName}Model.js`
+    });
+}
+
+module.exports = createModelFile;

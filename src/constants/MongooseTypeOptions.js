49b63ae6e900829b33545b1c2dd3ba85c18a4c3b
Kevin da Silva <kevin.silva@sensedia.com>
2021-04-22 17:06:18 -0300

    Create a constant for mongoose type options

---
+++ src/constants/MongooseTypeOptions.js
@@ -0,0 +1,6 @@
+const typeOptions = ["String", "Number", 
+              "Date", "Buffer", "Boolean", 
+              "Mixed", "ObjectId", "Array", 
+              "Decimal128", "Map", "Schema"];
+
+module.exports = typeOptions;

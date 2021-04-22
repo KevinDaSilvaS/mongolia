807964288f481709852ff6fa9750377271fe7971
Kevin da Silva <kevin.silva@sensedia.com>
2021-04-22 16:09:08 -0300

    Change connection to receive env vars

--- poc/connection.js
+++ poc/connection.js
@@ -1,12 +1,21 @@
+require('dotenv/config');
 const mongoose = require('mongoose');
 
+const {MONGO_USERNAME, MONGO_PASSWORD, MONGODB_HOST, MONGODB_PORT, MONGODB_NAME}  = process.env;
+const username = MONGO_USERNAME || "";
+const password = MONGO_PASSWORD || "";
+const host = MONGODB_HOST || "localhost";
+const port = MONGODB_PORT || "27017";
+const mongodbName = MONGODB_NAME || "admin";
+
 const connection = async () => {
-    //in local mode change mongo to localhost
-    return await mongoose.connect(`mongodb://kevin:123@localhost:27017/admin`, 
+
+    return await mongoose.connect(`mongodb://${username}:${password}@${host}:${port}/${mongodbName}`, 
     {useNewUrlParser: true, useUnifiedTopology: true},
     (err) => {
         if (err) {
            console.log(err); 
+           throw err;
         }
     });
 }

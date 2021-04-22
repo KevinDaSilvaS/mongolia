a9dd013c8468bec7b059466bd322cebfc716c84c
Kevin da Silva <kevin.silva@sensedia.com>
2021-04-22 11:38:01 -0300

    Add fields validations

---
+++ src/validations/models/ModelProperties.js
@@ -0,0 +1,64 @@
+const typeOptions = ["String", "Number", 
+              "Date", "Buffer", "Boolean", 
+              "Mixed", "ObjectId", "Array", 
+              "Decimal128", "Map", "Schema"];
+
+const sanitizeFields = (collectionProperties) => {
+    try {
+        const collectionFields = Object.values(collectionProperties);
+
+        const sanitizedFields = collectionFields.map((field) => validateProperties(field));
+        const keys = Object.keys(collectionProperties);
+
+        let finalModelObj = {};
+        for (let index = 0; index < keys.length; index++) {
+            finalModelObj[keys[index]] = sanitizedFields[index];
+        }
+        
+        return finalModelObj;
+    } catch (error) {
+        throw error;
+    }
+    
+}
+
+const validateProperties = (fieldProperty) => {
+    try {
+        const type = validateType(fieldProperty);
+        const required = validateRequired(fieldProperty['required']);
+
+        const properties = {
+            type,
+            required,
+        }
+        return properties;
+
+    } catch (error) {
+        throw error
+    }
+}
+
+const validateType = ({type}) => {
+    if(type && typeOptions.includes(type))
+        return type
+    throw `Type not properly set. Accepted values(${typeOptions.toString()})`;
+}
+
+const validateRequired = (required) => {
+    if(!required)
+        return false
+
+    if(typeof required == typeof false)
+        return required
+    throw "Required not properly set. Accepted values(true, false)";
+}
+
+const validateDefault = (defaultValue) => {
+    if(!defaultValue)
+        return undefined
+
+    return defaultValue;
+
+}
+
+module.exports = sanitizeFields;

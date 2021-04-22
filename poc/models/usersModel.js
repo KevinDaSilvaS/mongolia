b8c4652366e5999122ff6c99a5dc2ba00134daa8
Kevin da Silva <kevin.silva@sensedia.com>
2021-04-22 08:56:00 -0300

    Finish POC from data gatewat

--- poc/models/usersModel.js
+++ poc/models/usersModel.js
@@ -1,7 +1,7 @@
 
         const mongoose = require('mongoose');
     
-        const usersSchema = new mongoose.Schema({name:{type:String,required:true},age:{type:Number},status:{type:String}});
+        const usersSchema = new mongoose.Schema({name:{type:String,required:true},age:{type:Number}});
         
         const usersModel = mongoose.model("users", usersSchema);
         
--- poc/routes.js
+++ poc/routes.js
@@ -11,6 +11,17 @@ app.use(bodyParser.json());
 fs = require('fs');
 
 const port = 3170
+let token = undefined;
+
+app.post('/auth', (req, res) => {
+    const {username, password} = req.body;
+    if(username == "kevin" && password == "123" && !token){
+        token = "123456A"
+        return res.send({token})
+    }
+        
+    res.send({error: "Credentials dont match or auth token was already set"})
+})
 
 app.post('/tables', (req, res) => {
     const {collectionName, collectionProperties} = req.body;
@@ -40,7 +51,7 @@ app.post('/tables', (req, res) => {
     fields[collectionName] = Object.keys(collectionProperties); 
     //validateProperties(collectionProperties);
 
-    res.send('Hello World!')
+    res.status(201).send('Hello World!')
 })
 
 app.post('/tables/:tableName', async (req, res) => {
@@ -63,31 +74,60 @@ app.post('/tables/:tableName', async (req, res) => {
     res.status(400).send({error: inserted})
 })
 
-app.patch('/tables/:tableName', (req, res) => {
-    if(objCollections[collectionName]){
-        res.status(400).send('Table already exists')
+app.patch('/tables/:tableName', async (req, res) => {
+    const { tableName } = req.params;
+    if(!objCollections[tableName]){
+        res.status(400).send({error: 'Table doesnt exists'})
         return;
     }
-    const { tableName } = req.params;
-    res.send({tableName});
+
+    const path = require(objCollections[tableName]);
+    const operations = new Operations(path);
+    const registers = await operations.update(req.query, req.body);
+    if(!registers) {
+        res.status(400).send({error: registers})
+        return
+    }
+    
+    res.status(204).send();
 })
 
-app.get('/tables/:tableName', (req, res) => {
-    if(objCollections[collectionName]){
-        res.status(400).send('Table already exists')
+app.get('/tables/:tableName', async (req, res) => {
+
+    const { tableName } = req.params;
+    if(!objCollections[tableName]){
+        res.status(400).send({error: 'Table doesnt exists'})
         return;
     }
-    const { tableName } = req.params;
-    res.send({tableName});
+
+    const {limit, page, ...rest} = req.query;
+    const path = require(objCollections[tableName]);
+    const operations = new Operations(path);
+    const registers = await operations.getPaginated(rest, page, limit);
+    if(!registers) {
+        res.status(400).send({error: registers})
+        return
+    }
+    
+    res.send(registers);
 })
 
-app.delete('/tables/:tableName', (req, res) => {
-    if(objCollections[collectionName]){
-        res.status(400).send('Table already exists')
+app.delete('/tables/:tableName', async (req, res) => {
+    const { tableName } = req.params;
+    if(!objCollections[tableName]){
+        res.status(400).send({error: 'Table doesnt exists'})
         return;
     }
-    const { tableName } = req.params;
-    res.send({tableName});
+
+    const path = require(objCollections[tableName]);
+    const operations = new Operations(path);
+    const registers = await operations.delete(req.query);
+    if(!registers) {
+        res.status(400).send({error: registers})
+        return
+    }
+    
+    res.status(204).send();
 })
 
 

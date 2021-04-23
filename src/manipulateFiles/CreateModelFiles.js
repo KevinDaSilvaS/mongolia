b5ac9f2613070867e8a76886e0e665f08d6a6f8a
Kevin da Silva <kevin.silva@sensedia.com>
2021-04-23 10:06:23 -0300

    Update model file creation

--- src/manipulateFiles/CreateModelFiles.js
+++ src/manipulateFiles/CreateModelFiles.js
@@ -1,7 +1,7 @@
 fs = require('fs');
 
 const createModelFile = ({collectionName, collectionProperties, path, addressPath}) => {
-    let parsedCollectionProperties = JSON.stringify((collectionProperties)).replace(/"/g, '');
+    let parsedCollectionProperties = JSON.stringify(collectionProperties).toString().replace(/"/g, '');
     const contentFile = `
         const mongoose = require('mongoose');
     
@@ -16,6 +16,7 @@ const createModelFile = ({collectionName, collectionProperties, path, addressPat
         if (err) throw err;
         return `${addressPath}/${collectionName}Model.js`
     });
+    return `${addressPath}/${collectionName}Model.js`
 }
 
 module.exports = createModelFile;

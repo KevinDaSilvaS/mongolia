a7f5f06591fb6f0f4904adaf148072f0ebce3524
Kevin da Silva <kevin.silva@sensedia.com>
2021-04-23 10:07:27 -0300

    Update token

--- src/memory-manager/token.js
+++ src/memory-manager/token.js
@@ -3,8 +3,8 @@ const { v4: uuidv4 } = require('uuid');
 createToken = () => uuidv4();
 
 const credentials = {
-    username: "kevin",
-    password: "123",
+    username: undefined,
+    password: undefined,
     token: undefined,
     createToken
 }

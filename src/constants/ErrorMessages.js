4c3c27f9ea13cdf10f750d98d1f94e34608e0264
Kevin da Silva <kevin.silva@sensedia.com>
2021-04-23 11:54:53 -0300

    Add new custom error messages

--- src/constants/ErrorMessages.js
+++ src/constants/ErrorMessages.js
@@ -3,4 +3,5 @@ module.exports = {
     AUTH_TOKEN_ALREADY_SET: "Auth token already set. To reset token restart mongolia.",
     AUTH_TOKEN_NOT_PROPERLY_SET: "Auth token not properly set. Token doens't match.",
     COLLECTION_ALREADY_EXISTS: "Collection already exists.",
+    COLLECTION_DOES_NOT_EXIST: "Collection doesn't exists.",
 }

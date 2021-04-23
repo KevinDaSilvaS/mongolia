eec64486f037718c85a2b4c09561f884b57c7c06
Kevin da Silva <kevin.silva@sensedia.com>
2021-04-23 11:54:36 -0300

    Adjust controllers

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,15 @@
 const Authenticate = require('./auth/Authenticate');
-const Other = require('./auth/Other');
 const CreateCollection = require('./collections/CreateCollection');
+const InsertInCollection = require('./collections/InsertInCollection');
+const DeleteInCollection = require('./collections/DeleteInCollection');
+const GetInCollection = require('./collections/GetInCollection');
+const UpdateInCollection = require('./collections/UpdateInCollection');
 
 module.exports = {
     Authenticate,
-    Other,
-    CreateCollection
+    CreateCollection,
+    InsertInCollection,
+    DeleteInCollection,
+    GetInCollection,
+    UpdateInCollection
 }

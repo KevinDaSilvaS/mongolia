9a42f976e69848a114faeefbdb84c7c1a28c6a3e
Your Name <kevin.kds80@gmail.com>
2021-04-23 19:33:37 -0300

    Remove poc folder from project

--- poc/models/usersModel.js
+++
@@ -1,9 +0,0 @@
-
-        const mongoose = require('mongoose');
-    
-        const usersSchema = new mongoose.Schema({name:{type:String,required:true},age:{type:Number}});
-        
-        const usersModel = mongoose.model("users", usersSchema);
-        
-        module.exports = usersModel;
-    

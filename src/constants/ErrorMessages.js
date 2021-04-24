95cfb309edb87f8148f555634bbd21be3c2db547
Your Name <kevin.kds80@gmail.com>
2021-04-24 12:40:19 -0300

    Update error descriptions

--- src/constants/ErrorMessages.js
+++ src/constants/ErrorMessages.js
@@ -1,5 +1,5 @@
 module.exports = {
-    ERROR_AUTHENTICATING: "Credentials dont match.",
+    ERROR_AUTHENTICATING: "Credentials dont match. Check username and password",
     AUTH_TOKEN_ALREADY_SET: "Auth token already set. To reset token restart mongolia.",
     AUTH_TOKEN_NOT_PROPERLY_SET: "Auth token not properly set. Token doens't match.",
     COLLECTION_ALREADY_EXISTS: "Collection already exists.",

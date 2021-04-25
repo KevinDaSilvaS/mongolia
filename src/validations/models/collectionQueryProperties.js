406a0e65e467ca6becebeafd1b5a84056f736185
Your Name <kevin.kds80@gmail.com>
2021-04-25 19:10:47 -0300

    Add comparative query to mongolia as well as unit tests for the new feature

--- src/validations/models/collectionQueryProperties.js
+++ src/validations/models/collectionQueryProperties.js
@@ -5,17 +5,19 @@ sanitizeQuery = (value, property) => {
 
 const buildComparativeQuery = (fieldValue) => {
 
+    console.log(fieldValue)
     const queryElems = fieldValue.split("@>");
     const finalValue = {};
     queryElems.map((elem) => {
-        if(elem.match(/GT/g))
-            return finalValue.$gt = sanitizeQuery(elem, /GT/g);
-        if(elem.match(/LT/g))
-            return finalValue.$lt = sanitizeQuery(elem, /LT/g);
+        
         if(elem.match(/GTE/g))
             return finalValue.$gte = sanitizeQuery(elem, /GTE/g);
         if(elem.match(/LTE/g))
             return finalValue.$lte = sanitizeQuery(elem, /LTE/g);
+        if(elem.match(/GT/g))
+            return finalValue.$gt = sanitizeQuery(elem, /GT/g);
+        if(elem.match(/LT/g))
+            return finalValue.$lt = sanitizeQuery(elem, /LT/g);
         if(elem.match(/EQ/g))
             return finalValue.$eq = sanitizeQuery(elem, /EQ/g);
     });
--- test/unit/validations/models/collectionQueryProperties.test.js
+++ test/unit/validations/models/collectionQueryProperties.test.js
@@ -28,4 +28,89 @@ describe('Sucess', () => {
  
         expect(sanitizedFields).toEqual(payload);
     });
+
+    test('Should sanitize payload fields with @>EQ', async () => {
+        payload.age = '@>EQ21';
+        const sanitizedFields = execute(payload, allowedFields);
+ 
+        const expectedPayload = {
+            _id: "123",
+            age: {
+                '$eq': "21"
+            },
+            name: "any_name"
+        }
+        expect(sanitizedFields).toEqual(expectedPayload);
+    });
+
+    test('Should sanitize payload fields with @>GT', async () => {
+        payload.age = '@>GT18';
+        const sanitizedFields = execute(payload, allowedFields);
+ 
+        const expectedPayload = {
+            _id: "123",
+            age: {
+                '$gt': "18"
+            },
+            name: "any_name"
+        }
+        expect(sanitizedFields).toEqual(expectedPayload);
+    });
+
+    test('Should sanitize payload fields with @>LT', async () => {
+        payload.age = '@>LT18';
+        const sanitizedFields = execute(payload, allowedFields);
+ 
+        const expectedPayload = {
+            _id: "123",
+            age: {
+                '$lt': "18"
+            },
+            name: "any_name"
+        }
+        expect(sanitizedFields).toEqual(expectedPayload);
+    });
+
+    test('Should sanitize payload fields with @>GTE', async () => {
+        payload.age = '@>GTE18';
+        const sanitizedFields = execute(payload, allowedFields);
+ 
+        const expectedPayload = {
+            _id: "123",
+            age: {
+                '$gte': "18"
+            },
+            name: "any_name"
+        }
+        expect(sanitizedFields).toEqual(expectedPayload);
+    });
+
+    test('Should sanitize payload fields with @>LTE', async () => {
+        payload.age = '@>LTE18';
+        const sanitizedFields = execute(payload, allowedFields);
+ 
+        const expectedPayload = {
+            _id: "123",
+            age: {
+                '$lte': "18"
+            },
+            name: "any_name"
+        }
+        expect(sanitizedFields).toEqual(expectedPayload);
+    });
+
+    test('Should sanitize payload fields with a combination @>LTE @>GTE', async () => {
+        payload.age = '@>LTE18@>GTE10';
+        const sanitizedFields = execute(payload, allowedFields);
+ 
+        const expectedPayload = {
+            _id: "123",
+            age: {
+                '$lte': "18",
+                '$gte': "10"
+            },
+            name: "any_name"
+        }
+        expect(sanitizedFields).toEqual(expectedPayload);
+    });
 });

553698d37f371d89e339ee4c05561d7492c496ca
Your Name <kevin.kds80@gmail.com>
2021-04-25 09:35:34 -0300

    Add comparative properties to query

--- src/validations/models/collectionQueryProperties.js
+++ src/validations/models/collectionQueryProperties.js
@@ -1,8 +1,40 @@
+sanitizeQuery = (value, property) => {
+    const rawValue = value.replace(property, '');
+    return rawValue;
+}
+
+const buildComparativeQuery = (fieldValue) => {
+
+    const queryElems = fieldValue.split("@>");
+    const finalValue = {};
+    queryElems.map((elem) => {
+        if(elem.match(/GT/g))
+            return finalValue.$gt = sanitizeQuery(elem, /GT/g);
+        if(elem.match(/LT/g))
+            return finalValue.$lt = sanitizeQuery(elem, /LT/g);
+        if(elem.match(/GTE/g))
+            return finalValue.$gte = sanitizeQuery(elem, /GTE/g);
+        if(elem.match(/LTE/g))
+            return finalValue.$lte = sanitizeQuery(elem, /LTE/g);
+        if(elem.match(/EQ/g))
+            return finalValue.$eq = sanitizeQuery(elem, /EQ/g);
+    });
+
+    if(Object.keys(finalValue).length > 0)
+        return finalValue;
+
+    return fieldValue;
+}
+
 module.exports = (fields, allowedFields) => {
     const arrFields = Object.keys(fields);
     allowedFields.push('_id');
     const sanitizedFields = arrFields.filter((field) => allowedFields.includes(field));
     const query = {};
-    sanitizedFields.map((field) => query[field] = fields[field])
+    sanitizedFields.map((field) => {
+        const fieldValue = buildComparativeQuery(fields[field]);
+
+        query[field] = fieldValue;
+    })
     return query;
 }
